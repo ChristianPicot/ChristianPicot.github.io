e43b8c948cdf80663b95d434f4080183717d7f1d
Paicot <ch81arg@yahoo.com.ar>
2024-12-18 23:01:20 -0300

    .png

--- script.js
+++ script.js
@@ -27,7 +27,7 @@ function updateBusPositions() {
 
                     // Add a moving icon (optional)
                     var busIcon = L.icon({
-                        iconUrl: `bondi${linea}`, //'bondi59.png', // Replace with the path to your icon
+                        iconUrl: `bondi${linea}.png`, //'bondi59.png', // Replace with the path to your icon
                         iconSize: [32, 32], // Adjust icon size as needed
                         iconAnchor: [16, 16], // Adjust icon anchor as needed
                         popupAnchor: [0, -16]
