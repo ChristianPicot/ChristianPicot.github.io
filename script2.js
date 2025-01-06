6cdd24595417d0ad61756645341b784c83c329f6
Paicot <ch81arg@yahoo.com.ar>
2025-01-05 23:04:44 -0300

    retrotraigo

--- script2.js
+++ script2.js
@@ -41,13 +41,14 @@ if (navigator.geolocation) {
 let busMarkers = {}; // Object to store bus markers
 
 function updateBusPositions() {
-    //fetch('https://paicot.pythonanywhere.com/bondis/') // Replace with your API endpoint
-    //fetch('https://181.170.199.213/bondis/') // Replace with your API endpoint    
-    fetch('https://e4fa-2800-21e0-3000-55a-2f93-9b57-486c-f84b.ngrok-free.app/bondis/', {
-      headers: {
-        'ngrok-skip-browser-warning': 'true' // Set your desired value here
-      }
-    }) // Replace with your API endpoint
+    fetch('https://paicot.pythonanywhere.com/bondis/') // Replace with your API endpoint
+    //fetch('https://181.170.199.213/bondis/') // esto no funciono pq no tengo certificado
+
+    //fetch('https://e4fa-2800-21e0-3000-55a-2f93-9b57-486c-f84b.ngrok-free.app/bondis/', {
+    //  headers: {
+    //    'ngrok-skip-browser-warning': 'true' // Set your desired value here
+    //  }
+    //}) // Replace with your API endpoint
         .then(response => response.json())
         .then(data => {
             data.forEach(bus => {
